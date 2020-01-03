0c305bbf7888fa8fd6ecad5122448f557d8a37a3
Kumar <ts-pratik.kumar@rakuten.com>
2020-01-03 12:55:32 +0530

    push

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Anguler-material';
+  notifications = 0;
+  showSpinner = false;
+
+  loadData() {
+
+    this.showSpinner = true
+    setTimeout(() => {
+      this.showSpinner = false;
+    }, 5000)
+  }
 }
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { 
+  MatButtonModule, MatButtonToggleModule, MatIconModule,
+  MatProgressSpinnerModule, MatToolbarModule, MatSidenavModule
+} from '@angular/material'
+import { MatBadgeModule } from '@angular/material/badge'
+
+const MaterialComponents = [
+  MatButtonModule, MatButtonToggleModule, MatIconModule,
+  MatBadgeModule, MatProgressSpinnerModule, MatToolbarModule,
+  MatSidenavModule
+]
+
+@NgModule({
+  imports: [MaterialComponents],
+  exports: [MaterialComponents]
+
+})
+export class MaterialModule { }
